fb021d1e27602f60757238973fc86e8e88858092
fleet <manavdeora221@gmail.com>
2026-05-27 23:36:44 +0530

    refactor: btn component

--- memoryticketapp/src/components/Buttons/BackBtn.jsx
+++ memoryticketapp/src/components/Buttons/BackBtn.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import styles from './Buttons.module.css'
 import { ChevronLeft } from 'lucide-react';
 
-const BackBtn = ({ name: btnName }) => {
+const BackBtn = ({ name: btnName, onClick, customStyle,type }) => {
     return (
-        <button className={styles['btn']}>
+        <button type={type} className={styles['btn']} onClick={onClick} style={customStyle}>
             {
-                btnName ? <span>{btnName}</span>: <>
-                <ChevronLeft/>
-                <span>Back</span>
+                btnName ? <span>{btnName}</span> : <>
+                    <ChevronLeft />
+                    <span>Back</span>
                 </>
             }
-            
         </button>
     )
 }
