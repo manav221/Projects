1b3249412de3aa643cf818fda9350a637ee4613f
fleet <manavdeora221@gmail.com>
2026-05-28 23:30:36 +0530

    feat: add routing

--- memoryticketapp/src/App.jsx
+++ memoryticketapp/src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import Home from './pages/Home'
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home.jsx'
+import Explore from './pages/Explore/Explore.jsx'
+import Chats from './pages/Chats/Chats.jsx'
 import CreateStub from './pages/CreateStub/CreateStub.jsx'
+import Profile from './pages/Profile/Profile.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import FloatingBtn from './components/FloatingMenu/FloatingBtn.jsx'
 
 const App = () => {
+  const [allStubData, setAllStubData] = useState([]);
+  useEffect(() => {
+    if (localStorage.getItem("allStubs")) {
+      let rawData = localStorage.getItem("allStubs");
+      let data = JSON.parse(rawData);
+      setAllStubData(data);
+    }
+  }, [allStubData.length])
   return (
-    <div>
-      {/* <Home/> */}
-      <CreateStub />
+    <div className={['app-content']}>
+      <Routes>
+        <Route path='/create-stub' element={<CreateStub allStubData={allStubData} setAllStub={setAllStubData} />} />
+        <Route path='/' element={<Home recieveData={allStubData} />} />
+        <Route path='/explore' element={<Explore />} />
+        <Route path='/chats' element={<Chats />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </div>
   )
 }
