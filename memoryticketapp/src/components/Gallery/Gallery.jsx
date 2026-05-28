cfc1aec3f912a80a9f352074c2379c006c393dfa
fleet <manavdeora221@gmail.com>
2026-05-28 23:32:32 +0530

    fix: layout & styling

--- memoryticketapp/src/components/Gallery/Gallery.jsx
+++ memoryticketapp/src/components/Gallery/Gallery.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import styles from './Gallery.module.css'
 import TicketStub from '../TicketStub/TicketStub.jsx'
 
-const Gallery = () => {
+const Gallery = ({ stubData }) => {
     return (
         <div className={styles['all-stubs']}>
-            <TicketStub />
+            {   
+                stubData.length <= 0 ? <div className={styles['initial-txt']}>
+                    <span>Create Your First Stub</span>
+                </div> :
+                stubData.map((stub) => {
+                    return <TicketStub data={stub} />
+                })
+            }
         </div>
     )
 }
--- memoryticketapp/src/components/Navbar/Navbar.jsx
+++ memoryticketapp/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import styles from './Navbar.module.css'
-import { Bell } from 'lucide-react';
-
+import { Bell, Ticket } from 'lucide-react';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <div className={styles['nav-bar']}>
-      <div className={styles["profile-icon"]}>
-        <div className={styles["img-container"]}>
-          <img src="https://plus.unsplash.com/premium_photo-1675200124904-dfadce24119f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        </div>
-      </div>
+      <Link to={"/create-stub"} className={styles['new-stub']}>
+        <Ticket size={25} style={{ transform: "rotate(90deg)" }} />
+      </Link>
       <div className={styles["app-name"]}>Memory Stubs</div>
       <div className={styles['app-notification']}>
-          <Bell />
+        <Bell />
         <div className={styles['badge']}>
           <span></span>
         </div>
